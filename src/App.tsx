8c05f6a648ac82b2a81a8d2e2365b3e1b671af0a
kongminh113 <kongminh113@gmail.com>
2020-02-20 10:25:59 +0900

    Added jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import './App.css'
 import CakeContainer from './CakeContainer'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import Button from './component/button/button'
 
 function App() {
   return (
     <Provider store= {store}>
       <div className="App">
-        <CakeContainer />
+        <header className="App">
+          <CakeContainer />
+          <Button label="click me please"/>
+        </header>
       </div>
     </Provider>
   );
